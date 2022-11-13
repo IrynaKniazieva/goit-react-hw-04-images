6d47894121d8b6976beb181288c5988853e85c41
IrynaKniazieva <gleb0812@gmail.com>
2022-11-14 00:04:35 +0200

    Searchbar on hooks

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,55 +1,96 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+// import PropTypes from 'prop-types';
 import styles from './Searchbar.module.css';
 import { GoSearch } from 'react-icons/go';
 // import { toast } from 'react-toastify';
 
-class Searchbar extends React.Component {
-  state = {
-    query: '',
-  };
+export default function Searchbar ({ onSubmit }) {
 
-  static propTypes = {
-    onSubmit: PropTypes.func,
-  };
+  const [query, setQuery] = useState('');
 
-  handleSearchQueryChange = e => {
-    this.setState({ query: e.currentTarget.value.toLowerCase() });
-  };
+  const handleSearchQueryChange = e => setQuery(e.currentTarget.value.toLowerCase());
 
-  handleSearchQuerySubmit = e => {
+  const handleSearchQuerySubmit = e => {
     e.preventDefault();
-    if (this.state.query.trim() === '') {
+    if (query.trim() === '') {
       alert('Enter a valid name');
       return;
-    }
-    this.props.onSubmit(this.state.query);
-    this.setState({ query: '' });
-  };
-  render() {
-    return (
-      <header className={styles.searchbar}>
-        <form
-          className={styles.formHeader}
-          onSubmit={this.handleSearchQuerySubmit}
-        >
-          <button className={styles.buttonHeader} type="submit">
-            <GoSearch />
-          </button>
-
-          <input
-            className={styles.inputHeader}
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            value={this.state.query}
-            onChange={this.handleSearchQueryChange}
-          />
-        </form>
-      </header>
-    );
+      }
+    onSubmit(query);
+    setQuery('')
   }
+
+
+  return (
+    <header className={styles.searchbar}>
+      <form
+        className={styles.formHeader}
+        onSubmit={handleSearchQuerySubmit}
+      >
+        <button className={styles.buttonHeader} type="submit">
+          <GoSearch />
+        </button>
+
+        <input
+          className={styles.inputHeader}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          value={query}
+          onChange={handleSearchQueryChange}
+        />
+      </form>
+    </header>
+  );
 }
 
-export default Searchbar;
+// class Searchbar extends React.Component {
+//   state = {
+//     query: '',
+//   };
+
+//   static propTypes = {
+//     onSubmit: PropTypes.func,
+//   };
+
+//   handleSearchQueryChange = e => {
+//     this.setState({ query: e.currentTarget.value.toLowerCase() });
+//   };
+
+//   handleSearchQuerySubmit = e => {
+//     e.preventDefault();
+//     if (this.state.query.trim() === '') {
+//       alert('Enter a valid name');
+//       return;
+//     }
+//     this.props.onSubmit(this.state.query);
+//     this.setState({ query: '' });
+//   };
+//   render() {
+//     return (
+//       <header className={styles.searchbar}>
+//         <form
+//           className={styles.formHeader}
+//           onSubmit={this.handleSearchQuerySubmit}
+//         >
+//           <button className={styles.buttonHeader} type="submit">
+//             <GoSearch />
+//           </button>
+
+//           <input
+//             className={styles.inputHeader}
+//             type="text"
+//             autoComplete="off"
+//             autoFocus
+//             placeholder="Search images and photos"
+//             value={this.state.query}
+//             onChange={this.handleSearchQueryChange}
+//           />
+//         </form>
+//       </header>
+//     );
+//   }
+// }
+
+// export default Searchbar;
