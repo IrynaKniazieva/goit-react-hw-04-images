66211396344e1dab329c1365b99429080de82fb6
IrynaKniazieva <gleb0812@gmail.com>
2022-11-15 01:27:35 +0200

    hooks APP work

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
-import React from "react"
-// import React, { useState, useEffect } from "react"
+// import React from "react"
+import React, { useState, useEffect } from "react"
 import { Section } from './App.styled';
 import Modal from "../Modal/Modal";
 import Searchbar from "../Searchbar/Searchbar";
@@ -9,120 +9,66 @@ import Loader from "../Loader/Loader";
 import { fetchImagesWithQuery } from "services/API";
 // import { useEffect, useState } from 'react';
 
-// export default function App () {
-//   const [images, setImages] = useState([]);
-//   const [query, setQuery] = useState('');
-//   const [loading, setLoading] = useState(false);
-//   const [error, setError] = useState(null);
-//   const [page, setPage] = useState(1);
-//   const [largeImage, setLargeImage] = useState('');
-//   const [showButton, setShowButton] = useState(false);
-//   const [totalHits, setTotalHits] = useState(0);
+export default function App () {
+  const [images, setImages] = useState([]);
+  const [query, setQuery] = useState('');
+  // const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [page, setPage] = useState(1);
+  const [largeImage, setLargeImage] = useState('');
+  // const [showButton, setShowButton] = useState(false);
+  const [totalHits, setTotalHits] = useState(0);
+  const [status, setStatus] = useState('idle');
 
-//   useEffect(() => {
-//     fetchImagesWithQuery(query, page).then(res => {
-//       setImages(prevImages => [...prevImages, ...res.data.hits]);
-//       setTotalHits(res.data.totalHits);
-//       setQuery(query);
-//     }
-//     )
-//   },[query, page]);
-
-//    const handleFormSubmit = query => {
-//     setQuery(query);
-//     setPage(1);
-//     setImages([]);
-//   };
-
-
-//   return (
-//       <>
-//         <Searchbar onSubmit={handleFormSubmit} />
-//         {/* <p>{this.state.searchQuery}</p> */}
-
-//         {error && <p>Whoops, something went wrong: {error.message}</p>}
-        
-//         {loading && <p>Загружаю...</p>}
-//         {images.length > 0 && (
-//         // <p>{this.state.searchQuery}</p>
-//         <ImageGallery onSelect={this.ImageClick} images={images}/>
-//         )}
-//          {!loading && (
-//               <Button text="Load More" clickHandler={this.loadMore} />
-//             )}
-//         <div>
-//           {largeImage.length > 0 && (
-//             <Modal imageModal={largeImage} closeModal={this.onClose} />
-//           )}
-//         </div>
-//         {/* <ToastContainer autoClose={3000}/> */}
-//       </>
-//     );
-// }
-
-class App extends React.Component {
-  state = {
-    images: [],
-    status: 'idle',
-    loading: false,
-    error: null,
-    page: 1,
-    query: '',
-    largeImage: '',
-    showButton: false,
-    totalHits: 0,
-  };
-
-  async componentDidUpdate(prevProps, prevState) {
-    const prevQuery = prevState.query;
-    const prevPage = prevState.page;
-    const { query, page } = this.state;
-
-    if (prevQuery !== query || prevPage !== page) {
-      this.setState({ status: 'pending' });
-
+  useEffect(() => {
+    if (query === '') {
+      return;
+    }
+    setStatus('pending');
+    const fetchImages = async () => {
       try {
         const res = await fetchImagesWithQuery(query, page);
-
-        this.setState(prevState => ({
-          images: [...prevState.images, ...res.data.hits],
-          totalHits: res.data.totalHits,
-          query: query,
-          status: 'resolved',
-        }));
+        if (page === 1) {
+          setImages(res.data.hits)
+        } else {
+          setImages(prevState => [...prevState, ...res.data.hits]);
+        }
+          setTotalHits(res.data.totalHits);
+          setQuery(query);
+          setStatus('resolved');
+        
       } catch (error) {
-        this.setState({ error, status: 'rejected' });
+        setError( error);
+        setStatus('rejected');
       }
-    }
-  }
+    }; fetchImages()}, [query, page]
+    )
 
-  handleFormSubmit = query => {
-    this.setState({ query, page: 1, images: [] });
+
+  const handleFormSubmit = query => {
+    setQuery(query);
+    setPage(1);
+    setImages([]);
   };
 
-  loadMore = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
+  const loadMore = () => {
+    setPage(prevPage => prevPage + 1)
   };
 
-  ImageClick = largeImageURL => {
-    this.setState({ largeImage: largeImageURL });
+const imageClick = largeImageURL => {
+  setLargeImage(largeImageURL);
   };
 
-  onClose = () => {
-    this.setState({
-      largeImage: '',
-    });
+    const onClose = () => {
+      setLargeImage('')
   };
 
-  render() {
-    const { images, error, largeImage, status, totalHits } = this.state;
-    // ----початок пуста сторінка----
-    if (status === 'idle') {
-      return (
+ 
+
+  if (status === 'idle') {
+        return (
         <Section>
-          <Searchbar onSubmit={this.handleFormSubmit} />
+          <Searchbar onSubmit={handleFormSubmit} />
           {/* <MessageError message={"Введіть назву для пошуку"}/> */}
         </Section>
       );
@@ -131,8 +77,8 @@ class App extends React.Component {
     if (status === 'pending') {
       return (
         <Section>
-          <Searchbar onSubmit={this.handleFormSubmit} />
-          <ImageGallery onSelect={this.ImageClick} images={images} />
+          <Searchbar onSubmit={handleFormSubmit} />
+          <ImageGallery onSelect={imageClick} images={images} />
           <Loader />
         </Section>
       );
@@ -141,7 +87,7 @@ class App extends React.Component {
     if (status === 'rejected') {
       return (
         <Section>
-          <Searchbar onSubmit={this.handleFormSubmit} />
+          <Searchbar onSubmit={handleFormSubmit} />
           {error && <p>Whoops, something went wrong: {error.message}</p>}
         </Section>
       );
@@ -150,21 +96,129 @@ class App extends React.Component {
     if (status === 'resolved') {
       return (
         <Section>
-          <Searchbar onSubmit={this.handleFormSubmit} />
-          <ImageGallery onSelect={this.ImageClick} images={images} />
+          <Searchbar onSubmit={handleFormSubmit} />
+          <ImageGallery onSelect={imageClick} images={images} />
           {largeImage.length > 0 && (
-            <Modal imageModal={largeImage} closeModal={this.onClose} />
+            <Modal imageModal={largeImage} closeModal={onClose} />
           )}
           {images.length !== totalHits && (
-            <Button text="Load More..." clickHandler={this.loadMore} />
+            <Button text="Load More..." clickHandler={loadMore} />
           )}
         </Section>
       );
-    }
-  }
+}
 }
 
-export default App;
+
+ 
+
+// class App extends React.Component {
+//   state = {
+//     images: [],
+//     status: 'idle',
+//     // loading: false,
+//     error: null,
+//     page: 1,
+//     query: '',
+//     largeImage: '',
+//     // showButton: false,
+//     totalHits: 0,
+//   };
+
+//   async componentDidUpdate(prevProps, prevState) {
+//     const prevQuery = prevState.query;
+//     const prevPage = prevState.page;
+//     const { query, page } = this.state;
+
+//     if (prevQuery !== query || prevPage !== page) {
+//       this.setState({ status: 'pending' });
+
+//       try {
+//         const res = await fetchImagesWithQuery(query, page);
+
+//         this.setState(prevState => ({
+//           images: [...prevState.images, ...res.data.hits],
+//           totalHits: res.data.totalHits,
+//           query: query,
+//           status: 'resolved',
+//         }));
+//       } catch (error) {
+//         this.setState({ error, status: 'rejected' });
+//       }
+//     }
+//   }
+
+//   handleFormSubmit = query => {
+//     this.setState({ query, page: 1, images: [] });
+//   };
+
+//   loadMore = () => {
+//     this.setState(prevState => ({
+//       page: prevState.page + 1,
+//     }));
+//   };
+
+//   ImageClick = largeImageURL => {
+//     this.setState({ largeImage: largeImageURL });
+//   };
+
+//   onClose = () => {
+//     this.setState({
+//       largeImage: '',
+//     });
+//   };
+
+
+
+//   render() {
+//     const { images, error, largeImage, status, totalHits } = this.state;
+//     // ----початок пуста сторінка----
+//     if (status === 'idle') {
+//       return (
+//         <Section>
+//           <Searchbar onSubmit={this.handleFormSubmit} />
+//           {/* <MessageError message={"Введіть назву для пошуку"}/> */}
+//         </Section>
+//       );
+//     }
+//     // -----Спинер/загрузка-----
+//     if (status === 'pending') {
+//       return (
+//         <Section>
+//           <Searchbar onSubmit={this.handleFormSubmit} />
+//           <ImageGallery onSelect={this.ImageClick} images={images} />
+//           <Loader />
+//         </Section>
+//       );
+//     }
+//     // ----якщо помилка-----
+//     if (status === 'rejected') {
+//       return (
+//         <Section>
+//           <Searchbar onSubmit={this.handleFormSubmit} />
+//           {error && <p>Whoops, something went wrong: {error.message}</p>}
+//         </Section>
+//       );
+//     }
+//     // ----правильний запрос, все працює-----
+//     if (status === 'resolved') {
+//       return (
+//         <Section>
+//           <Searchbar onSubmit={this.handleFormSubmit} />
+//           <ImageGallery onSelect={this.ImageClick} images={images} />
+//           {largeImage.length > 0 && (
+//             <Modal imageModal={largeImage} closeModal={this.onClose} />
+//           )}
+//           {images.length !== totalHits && (
+//             <Button text="Load More..." clickHandler={this.loadMore} />
+//           )}
+//         </Section>
+//       );
+//     }
+//   }
+// }
+
+// export default App;
 
 
 
